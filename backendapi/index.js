f8c9103f4613b0412eeaa04806f45d0942ebd376
Sadan Subedi <sadansubedi@gmail.com>
2024-08-25 21:09:20 +0545

    connected to mongodb database

--- backendapi/index.js
+++ backendapi/index.js
@@ -1,7 +1,23 @@
 import express from 'express'//Ecmascript 6 
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+// console.log(process.env.MONGO_URL)
+
+
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log("connected to mongodb");
+}).catch((error)=>{
+    console.log(error)    
+})
+
+
+
 const app = express();
 
+
 const port = process.env.PORT || '3000';
+
 
 app.get('/',(req,res) =>{
     res.send("Hell sadan it's you first express code  ")
