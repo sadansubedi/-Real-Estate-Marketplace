2ed9f2ea2766b112768900f13d3ba6d68dcb2d5e
Sadan Subedi <sadansubedi@gmail.com>
2024-08-25 18:46:18 +0545

    create routes and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/Home.jsx'
+import Profile from './pages/Profile.jsx'
+import About from './pages/About.jsx'
+import Signin from './pages/Signin.jsx'
+import Signout from './pages/Signout.jsx'
 function App() {
   return (
-    <>
-    <h1 className='text-green-700 bg-slate-950'>app ho hai </h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/signin" element={<Signin/>}/>
+      <Route path="/signout" element={<Signout/>}/>
+    </Routes>
+    {/* <h1 className='text-green-700 bg-slate-950'>aba app hoina </h1> */}
+    </BrowserRouter>
   )
 }
 
