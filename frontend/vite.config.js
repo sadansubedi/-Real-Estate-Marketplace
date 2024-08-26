53bff7b4780c4d7ce07c7f5970bd063d9b2dbc49
Sadan Subedi <sadansubedi@gmail.com>
2024-08-26 17:39:43 +0545

    complete sign up functionality

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,45 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/api/, '/api'), // This line helps Vite correctly handle the path
+      },
+    },
+  },
+});
+
+
+
+// export default defineConfig({
+//   server:{
+//     proxy:{
+//       '/api':{
+//         target: 'http://localhost:3000/',
+//         changeOrigin:true,
+//         secure:false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
+
+/*
+Wre added this line to make post request for signup 1 hr :58 min
+server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false
+      },
+    },
+  },
+
+*/
