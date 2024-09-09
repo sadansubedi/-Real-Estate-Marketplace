a265c61dd5d544a814051fe5c296cb714dd30b46
Sadan Subedi <sadansubedi@gmail.com>
2024-09-09 21:27:25 +0545

    Create search Api route

--- backendapi/controllers/listingcontroller.js
+++ backendapi/controllers/listingcontroller.js
@@ -65,4 +65,53 @@ const getListing =async(req,res,next)=>{
     }
 }
 
-export {createListing,deleteListing,updateListing,getListing}
+//useful for searching functionality
+const getListings =async(req,res,next)=>{
+    try {
+        const limit = parseInt(req.query.limit) || 9 ;// default is 9
+        const startIndex = parseInt(req.query.startIndex) || 0;//default is 0 
+        
+        //if buyer doesnot choose offer ,furnished etc etc then we show all data while search 
+        let offer = req.query.offer ;
+        if (offer === undefined || offer === 'false') {
+            offer = {$in:[false,true]};            
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === 'false') {
+            furnished = {$in:[false,true]};
+        }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === 'false') {
+            parking = {$in:[false,true]};
+        }
+
+        let type = req.query.type;
+        if (type === undefined || type === 'all') {
+            type = {$in:['sale','rent']};
+        }
+
+        const searchTerm = req.query.searchTerm || '';
+        const sort = req.query.sort || 'createdAt';
+        const order = req.query.order || 'desc';
+
+        const listings = await Listing.find({
+            name: {$regex:searchTerm,$options : 'i'},//search in name ,offer,furnished etc etc options :'i' means don't care about lowercase or uppercase 
+            offer,
+            furnished,
+            parking,
+            type,
+
+        }).sort({[sort]:order})
+        .limit(limit)//by default limit is 9
+        .skip(startIndex);
+
+        return res.status(200).json(listings);
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export {createListing,deleteListing,updateListing,getListing,getListings}
