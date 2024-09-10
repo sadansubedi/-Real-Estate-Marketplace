100094c4f0437c812c3c1a38c5bcfc69b7c52994
Sadan Subedi <sadansubedi@gmail.com>
2024-09-10 22:24:39 +0545

    Create the listing item component and show listings

--- frontend/src/pages/Listing.jsx
+++ frontend/src/pages/Listing.jsx
@@ -92,7 +92,7 @@ const Listing=()=> {
             <p className='text-2xl font-semibold'>
               {listing.name} - ${' '}
               {listing.offer
-                ? listing.discountPrice.toLocaleString('en-US')
+                ? <span>{listing.discountPrice.toLocaleString('en-US')} off</span>
                 : listing.regularPrice.toLocaleString('en-US')}
               {listing.type === 'rent' && ' / month'}
             </p>
@@ -106,7 +106,7 @@ const Listing=()=> {
               </p>
               {listing.offer && (
                 <p className='bg-green-900 w-full max-w-[200px] text-white text-center p-1 rounded-md'>
-                  ${+listing.regularPrice - +listing.discountPrice} OFF
+                  ${+listing.regularPrice - +listing.discountPrice} only
                 </p>
               )}
             </div>
